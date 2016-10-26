2a6f9341390d22eb027e1841cd2ebd53d099cc8e
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-26 14:56:48 +0200

    Update Pedestrian.java

--- Pedestrian.java
+++ Pedestrian.java
@@ -1,19 +1,19 @@
 package streetsimulator;
 
-import java.util.Random;
-
 public class Pedestrian extends RoadUser{
     private final static char SYMBOL = '*';
-    private final static int speed = 1;
-    public int x, y;
-    public Pedestrian(){
-        x = 0;
-        y = 0;
+    private final static int SPEED = 1;
+    private int x, y;
+    public Pedestrian(int x, int y){
+        super();
+        this.x = x;
+        this.y = y;
         board[y][x] = SYMBOL;
     }
     public void move(){
-        //System.out.printf("%5c%3c",SYMBOL, SYMBOL);
-        super.move(x, y, SYMBOL, speed);
+        int[] newXY = super.move(x, y, SYMBOL, SPEED);
+        x = newXY[0];
+        y = newXY[1];
     }
     public void przedstawSie(){
         System.out.print("Jestem pieszym");
