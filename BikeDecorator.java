9d4a6ea18c4578ebdb6cc3a89fc13e85190ef35b
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-26 14:59:58 +0200

    Update BikeDecorator.java

--- BikeDecorator.java
+++ BikeDecorator.java
@@ -2,11 +2,19 @@ package streetsimulator;
 
 public class BikeDecorator extends VehicleDecorator{
     private boolean using = false;
+    private final static char SYMBOL = '^';
+    private final static int SPEED = 2;
+    private int x, y;
     public BikeDecorator(RoadUser user){
         super(user);
+        x = 0;
+        y = 0;
+       // board[y][x] = SYMBOL;
     }
     public void move(){
-        
+        int[] newXY = super.move(x, y, SYMBOL, SPEED);
+        x = newXY[0];
+        y = newXY[1];
     }
     public void przedstawSie(){
         user.przedstawSie();
