5a6bca4ced5a4835029d1acb50c914d4b9c89f38
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-26 11:11:52 +0200

    Update RoadUser.java

--- RoadUser.java
+++ RoadUser.java
@@ -1,7 +1,50 @@
 package streetsimulator;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public abstract class RoadUser {
-    protected char[][] board = new char[120][30];
-    public void move(){}
+    protected char[][] board = new char[30][120];
+    public RoadUser(){
+        for(int i = 0; i < board.length; i++)
+            for(int k = 0; k < board[i].length; k++)
+                board[i][k] = ' ';
+    }
+    public void move(int x, int y, char symbol, int speed){
+        boolean moved = false;
+        board[y][x] = ' ';
+        Random ran = new Random();
+        int direction; 
+        do{
+            direction = ran.nextInt(4);
+            if(direction == 0 && y - 1 >= 0){
+                y -= speed;
+                board[y][x] = symbol;
+                moved = true;
+            }else{
+                if(direction == 1 && x + 1 <= 120){
+                    x += speed;
+                    board[y][x] = symbol;
+                    moved = true;
+                }else{
+                    if(direction == 2 && y + 1 <= 30){
+                        y += speed;
+                        board[y][x] = symbol;
+                        moved = true;
+                    }else{
+                        if(x - 1 >= 0){
+                            x += speed;
+                            board[y][x] = symbol;
+                            moved = true;
+                        }
+                    }
+                }
+            }
+        }while(!moved);
+    }
     public void przedstawSie(){}
+    public char[][] getBoard(){
+        return board;
+    }
 }
+
