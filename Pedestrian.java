834fbcb7b4fd5fc20515fa06ee996b4f5de71d69
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 14:11:51 +0200

    Update Pedestrian.java
    dodanie metod zwracajacych, sprawdzanie czy wystapil wypadek

--- Pedestrian.java
+++ Pedestrian.java
@@ -5,15 +5,29 @@ public class Pedestrian extends RoadUser{
     private final static int SPEED = 1;
     private int x, y;
     public Pedestrian(int x, int y){
-        super();
         this.x = x;
         this.y = y;
         board[y][x] = SYMBOL;
     }
-    public void move(){
+    public boolean move(){
         int[] newXY = super.move(x, y, SYMBOL, SPEED);
         x = newXY[0];
         y = newXY[1];
+        if(isAccident(x,y)){
+            board[y][x] = 'W';
+            return false;
+        }
+        board[y][x] = SYMBOL;
+        return true;
+    }
+    public int getX(){
+        return x; 
+    }
+    public int getY(){
+        return y; 
+    }
+    public static int getSpeed(){
+        return SPEED;
     }
     public void przedstawSie(){
         System.out.print("Jestem pieszym");
