a96643cceaf8275007cae4ca12e8893cecd0640a
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 14:11:04 +0200

    Update CarDecorator.java
    dokonczenie metody move, dodanie metod zwracajacych

--- CarDecorator.java
+++ CarDecorator.java
@@ -2,11 +2,34 @@ package streetsimulator;
 
 public class CarDecorator extends VehicleDecorator{
     private boolean using = false;
-    public CarDecorator(RoadUser user){
+    private final static char SYMBOL = '$';
+    private final static int SPEED = 3;
+    private int x, y;
+    public CarDecorator(Pedestrian user){
         super(user);
+        x = user.getX(); 
+        y = user.getY();
+        board[y][x] = SYMBOL;
     }
-    public void move(){
-        
+    public boolean move(){
+        int[] newXY = super.move(x, y, SYMBOL, SPEED);
+        x = newXY[0];
+        y = newXY[1];
+        if(isAccident(x,y)){
+            board[y][x] = 'W';
+            return false;
+        }
+        board[y][x] = SYMBOL;
+        return true;
+    }
+    public int getX(){
+        return x; 
+    }
+    public int getY(){
+        return y; 
+    }
+    public static int getSpeed(){
+        return SPEED;
     }
     public void przedstawSie(){
         user.przedstawSie(); 
