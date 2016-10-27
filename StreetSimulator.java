9bfdc3ef6ecf46127e9efd49042a5f65b39bafff
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 20:10:05 +0200

    Update StreetSimulator.java
    przeniesienie metod pomocniczych do klasy GameManager

--- StreetSimulator.java
+++ StreetSimulator.java
@@ -1,65 +1,12 @@
 package streetsimulator;
 
-import java.util.Random;
-
 public class StreetSimulator {
-    public static void drawBoard(){
-        char[][] board = RoadUser.board;
-        for(int i = 0; i < board.length; i++){
-            for(int k = 0; k < board[i].length; k++)
-                System.out.print(board[i][k]);
-            System.out.println(); 
-        }
-    }
-    public static int takeNumberElements(){
-        return new Random().nextInt(3) + 1;
-    }
-    public static int[] takeXY(Class roadUserClass){
-        char[][] board = RoadUser.board;
-        Random rand = new Random();
-        int x, y, speed;
-        boolean enoughSpace;
-        if(roadUserClass.equals(Pedestrian.class)) speed = Pedestrian.getSpeed();
-        else if(roadUserClass.equals(BikeDecorator.class)) speed = BikeDecorator.getSpeed();
-        else speed = CarDecorator.getSpeed();
-        do{
-            enoughSpace = true;
-            x = rand.nextInt(120); 
-            y = rand.nextInt(30);
-            int i = x - speed;
-            do{
-                if(i >= 0 && i < 120 && board[y][i] != ' ') enoughSpace = false;
-                i++;
-            }while(enoughSpace && i <= x + speed);
-            i = y - speed;
-            if(enoughSpace)
-                do{
-                    if(i >= 0 && i < 30 && board[i][x] != ' ') enoughSpace = false;
-                    i++;
-                }while(enoughSpace && i <= y + speed);
-        }while(!enoughSpace);
-        return new int[] {x, y};
-    }
     public static void main(String[] args) {
-        Pedestrian[] walkers = new Pedestrian[takeNumberElements()];
-        int[] xy;
-        Random rand = new Random();
-        walkers[0] = new Pedestrian(rand.nextInt(120), rand.nextInt(30));
-        BikeDecorator[] bikes = new BikeDecorator[takeNumberElements()];
-        CarDecorator[] cars = new CarDecorator[takeNumberElements()];
-        for(int i = 1; i < walkers.length; i++){
-                xy = takeXY(Pedestrian.class);
-                walkers[i] = new Pedestrian(xy[0],xy[1]);
-        }
-        for(int i = 0; i < bikes.length; i++){
-            xy = takeXY(BikeDecorator.class);
-            bikes[i] = new BikeDecorator(new Pedestrian(xy[0],xy[1]));
-        }
-        for(int i = 0; i < cars.length; i++){
-            xy = takeXY(CarDecorator.class);
-            cars[i] = new CarDecorator(new Pedestrian(xy[0], xy[1]));
-        }
-        drawBoard(); 
+        GameManager.makeGame();
+        Pedestrian[] walkers = GameManager.getWalkers();
+        BikeDecorator[] bikes = GameManager.getBikes();
+        CarDecorator[] cars = GameManager.getCars();
+        GameManager.drawBoard(); 
         boolean done = true;
         do{
             System.out.println("----------------------");
@@ -87,8 +34,8 @@ public class StreetSimulator {
                 }
             }
             if(!done) break;
-            drawBoard(); 
+            GameManager.drawBoard(); 
         }while(true);
-        drawBoard();
+        GameManager.drawBoard();
     }
 }
