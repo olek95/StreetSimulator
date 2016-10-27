1c06f74afbc6cce875c3c471b8752526777af4c1
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 14:10:24 +0200

    Update BikeDecorator.java
    dodanie metod zwracajacych oraz wyjścia z rowera

--- BikeDecorator.java
+++ BikeDecorator.java
@@ -5,16 +5,38 @@ public class BikeDecorator extends VehicleDecorator{
     private final static char SYMBOL = '^';
     private final static int SPEED = 2;
     private int x, y;
-    public BikeDecorator(RoadUser user){
+    public BikeDecorator(Pedestrian user){
         super(user);
-        x = 0;
-        y = 0;
-       // board[y][x] = SYMBOL;
+        x = user.getX();
+        y = user.getY();
+        board[y][x] = SYMBOL;
+        using = true;
     }
-    public void move(){
+    public boolean move(){
         int[] newXY = super.move(x, y, SYMBOL, SPEED);
         x = newXY[0];
         y = newXY[1];
+        if(isAccident(x,y)){
+            board[y][x] = 'W';
+            return false;
+        }
+        board[y][x] = SYMBOL;
+        return true;
+    }
+    public boolean getOff(){
+        using = false;
+        boolean done = ((Pedestrian)user).move(); 
+        System.out.println("PIESZY");
+        return done;
+    }
+    public int getX(){
+        return x; 
+    }
+    public int getY(){
+        return y; 
+    }
+    public static int getSpeed(){
+        return SPEED;
     }
     public void przedstawSie(){
         user.przedstawSie();
