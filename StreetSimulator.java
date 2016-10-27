db395d4874856ecd05e4577f7cbb3779ab698874
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 23:08:06 +0200

    Update StreetSimulator.java
    zamiana kolejności wypisywania użytkowników drogi, zapis użytkowników drogi w listach

--- StreetSimulator.java
+++ StreetSimulator.java
@@ -1,35 +1,41 @@
 package streetsimulator;
 
+import java.util.ArrayList;
+
 public class StreetSimulator {
     public static void main(String[] args) {
         GameManager.makeGame();
-        Pedestrian[] walkers = GameManager.getWalkers();
-        BikeDecorator[] bikes = GameManager.getBikes();
-        CarDecorator[] cars = GameManager.getCars();
+        ArrayList<Pedestrian> walkers = GameManager.getWalkers();
+        ArrayList<BikeDecorator> bikes = GameManager.getBikes();
+        ArrayList<CarDecorator> cars = GameManager.getCars();
         GameManager.drawBoard(); 
         boolean done = true;
         do{
             System.out.println("----------------------");
-            for(Pedestrian walker : walkers){
-                done = walker.move(); 
-                if(!done) {
-                    System.out.println("PIESZY");
-                    break;
-                }
-            }
-            if(!done) break;
             for(BikeDecorator bike : bikes){
-                done = bike.move(); 
-                if(!done) {
-                    System.out.println("ROWER");
-                    break;
+                if(bike.getSymbol() != '>'){
+                    done = bike.move();
+                    if(!done) {
+                        System.out.println("ROWER");
+                        break;
+                    }
                 }
             }
             if(!done) break;
             for(CarDecorator car : cars){
-                car.move();
+                if(car.getSymbol() != ']'){
+                    car.move();
+                    if(!done) {
+                        System.out.println("AUTO");
+                        break;
+                    }
+                }
+            }
+            if(!done) break;
+            for(Pedestrian walker : walkers){
+                done = walker.move(); 
                 if(!done) {
-                    System.out.println("AUTO");
+                    System.out.println("PIESZY");
                     break;
                 }
             }
