f4ba36cb169134605978130791c2074175e85506
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-27 23:10:25 +0200

    Update BikeDecorator.java
    Zmiana koloru

--- BikeDecorator.java
+++ BikeDecorator.java
@@ -1,30 +1,28 @@
 package streetsimulator;
 
 public class BikeDecorator extends VehicleDecorator{
-    private boolean using = false;
-    private final static char SYMBOL = '^';
+    private char symbol = '<';
     private final static int SPEED = 2;
     private int x, y;
     public BikeDecorator(Pedestrian user){
         super(user);
         x = user.getX();
         y = user.getY();
-        GameManager.changeBoardField(x, y, SYMBOL);
-        using = true;
+        GameManager.changeBoardField(x, y, symbol);
     }
     public boolean move(){
-        int[] newXY = super.move(x, y, SYMBOL, SPEED);
+        int[] newXY = super.move(x, y, symbol, SPEED);
         x = newXY[0];
         y = newXY[1];
         if(isAccident(x,y)){
             GameManager.changeBoardField(x, y, 'W');
             return false;
         }
-        GameManager.changeBoardField(x, y, SYMBOL);
+        GameManager.changeBoardField(x, y, symbol);
         return true;
     }
     public boolean getOff(){
-        using = false;
+        //using = false;
         boolean done = ((Pedestrian)user).move(); 
         System.out.println("PIESZY");
         return done;
@@ -38,8 +36,11 @@ public class BikeDecorator extends VehicleDecorator{
     public static int getSpeed(){
         return SPEED;
     }
-    public boolean isUsing(){
-        return using;
+    public void setSymbol(char symbol){
+        this.symbol = symbol;
+    }
+    public char getSymbol(){
+        return symbol;
     }
     public void przedstawSie(){
         user.przedstawSie();
