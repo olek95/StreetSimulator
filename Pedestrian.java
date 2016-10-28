62739c3ec672c0ce22680c9a86df00103cd2542e
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-28 14:16:23 +0200

    Update Pedestrian.java
    dokonczenie wsiadania

--- Pedestrian.java
+++ Pedestrian.java
@@ -1,5 +1,7 @@
 package streetsimulator;
 
+import java.util.ArrayList;
+
 public class Pedestrian extends RoadUser{
     private final static char SYMBOL = '*';
     private final static int SPEED = 1;
@@ -13,27 +15,42 @@ public class Pedestrian extends RoadUser{
         int[] newXY = super.move(x, y, SYMBOL, SPEED);
         x = newXY[0];
         y = newXY[1];
-        if(isAccident(x,y) && !isEmptyVehicleNear()){
-            GameManager.changeBoardField(x, y, 'W');
-            return false;
-        }else{
-            if(isAccident(x,y) && isEmptyVehicleNear())
-                if(GameManager.getBoard()[y][x] == '>'){
-                    System.out.println("WSIADLO");
-                    for(BikeDecorator bike : GameManager.getBikes())
-                        if(bike.getX() == x && bike.getY() == y){
-                            bike.setSymbol('<');
-                            GameManager.changeBoardField(x, y, '<');
-                        }
-                }else{
-                    for(CarDecorator car : GameManager.getCars())
-                        if(car.getX() == x && car.getY() == y){
-                            car.setSymbol('[');
-                            GameManager.changeBoardField(x, y, '[');
-                        }
+        if(isAccident(x,y)){
+            if(!isEmptyVehicleNear()){
+                GameManager.changeBoardField(x, y, 'W');
+                return false;
+            }else{
+                if(isEmptyVehicleNear()){
+                    boolean found = false;
+                    int i = 0;
+                    if(GameManager.getBoard()[y][x] == '>'){
+                        System.out.println("WSIADLO ROWER");
+                        ArrayList<BikeDecorator> bikes = GameManager.getBikes();
+                        BikeDecorator bike;
+                        do{
+                            bike = bikes.get(i);
+                            if(bike.getX() == x && bike.getY() == y) 
+                                found = true;
+                            if(!found) i++;
+                        }while(!found);
+                        bikes.set(i, new BikeDecorator(this));
+                        GameManager.getWalkers().remove(this);
+                    }else{
+                        System.out.println("WSIADLO AUTO");
+                        ArrayList<CarDecorator> cars = GameManager.getCars();
+                        CarDecorator car;
+                        do{
+                            car = cars.get(i);
+                            if(car.getX() == x && car.getY() == y)
+                                found = true;
+                            if(!found) i++;
+                        }while(!found);
+                        cars.set(i, new CarDecorator(this));
+                        GameManager.getWalkers().remove(this);
+                    }
                 }
-            else GameManager.changeBoardField(x, y, SYMBOL);
-        }
+            }
+        }else GameManager.changeBoardField(x, y, SYMBOL);
         return true;
     }
     private boolean isEmptyVehicleNear(){
