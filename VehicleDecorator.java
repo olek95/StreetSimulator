8dd05d7b36296cb1a1e462c85b3e23802f22a600
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-28 20:23:12 +0200

    Update VehicleDecorator.java

--- VehicleDecorator.java
+++ VehicleDecorator.java
@@ -1,12 +1,20 @@
 package streetsimulator;
 
 public abstract class VehicleDecorator extends RoadUser{
-    protected RoadUser user;
+    protected Pedestrian user;
+    protected int milage;
+    protected char symbol;
     public VehicleDecorator(RoadUser user){
-        this.user = user;
+        this.user = (Pedestrian)user;
     }
     public int[] move(int x, int y, char symbol, int speed){
         int[] newXY = super.move(x, y, symbol, speed);
         return newXY;
     } 
+    public void changeMilage(int milage){
+        this.milage += milage;
+    }
+    public int getMilage(){
+        return milage;
+    }
 }
