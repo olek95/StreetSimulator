648de6a1ff7f2d27b4ea52f82a725939f1fc956c
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-29 19:03:19 +0200

    Update BikeDecorator.java
    dodanie komentarzy, zmiana wartości zwracanej w metodzie getOff

--- BikeDecorator.java
+++ BikeDecorator.java
@@ -1,5 +1,9 @@
 package streetsimulator;
 
+/**
+ * Obiekt klasy <code>BikeDecorator</code> reprezentuje rower. 
+ * @author AleksanderSklorz
+ */
 public class BikeDecorator extends VehicleDecorator{
     private final static int SPEED = 2;
     public BikeDecorator(Pedestrian user){
@@ -9,6 +13,10 @@ public class BikeDecorator extends VehicleDecorator{
         symbol = '<';
         GameManager.changeBoardField(x, y, symbol);
     }
+    /**
+     * Przemieszcza rower w losowym kierunku. Posiada możliwość sprawdzenia czy nastąpił wypadek. 
+     * @return true jeśli przemieszczenie się udało, false jeśli doszło do wypadku. 
+     */
     public boolean move(){
         int[] newXY = super.move(x, y, symbol, SPEED);
         x = newXY[0];
@@ -20,15 +28,23 @@ public class BikeDecorator extends VehicleDecorator{
         GameManager.changeBoardField(x, y, symbol);
         return true;
     }
+    /**
+     * Pozwala na opuszczenie rowera. 
+     * @return true jeśli po opuszczeniu rowera pieszy nie wpada na inny element, false w przeciwnym przypadku.
+     */
     public boolean getOff(){
         System.out.println("WYSIADL Z ROWERA");
         Pedestrian walker = new Pedestrian(x,y);
-        walker.move();
+        boolean done = walker.move();
         GameManager.getWalkers().add(walker);
         symbol = '>';
         GameManager.changeBoardField(x, y, symbol);
-        return true;
+        return done;
     }
+    /**
+     * Zwraca prędkość rowera. 
+     * @return prędkość rowera. 
+     */
     public static int getSpeed(){
         return SPEED;
     }
