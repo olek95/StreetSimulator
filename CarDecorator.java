6e95d9c510b486e818b2464a526f6575686005b5
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-29 19:03:47 +0200

    Update CarDecorator.java
    dodanie komentarzy

--- CarDecorator.java
+++ CarDecorator.java
@@ -1,5 +1,9 @@
 package streetsimulator;
 
+/**
+ * Obiekt klasy <code>CarDecorator</code> reprezentuje samochód.
+ * @author AleksanderSklorz
+ */
 public class CarDecorator extends VehicleDecorator{
     private final static int SPEED = 3;
     public CarDecorator(Pedestrian user){
@@ -10,6 +14,10 @@ public class CarDecorator extends VehicleDecorator{
         milage = 0;
         GameManager.changeBoardField(x, y, symbol);
     }
+    /**
+     * Przemieszcza samochód w losowym kierunku. Posiada możliwość sprawdzenia czy nastąpił wypadek. 
+     * @return true jeśli przemieszczenie się udało, false jeśli doszło do wypadku. 
+     */
     public boolean move(){
         int[] newXY = super.move(x, y, symbol, SPEED);
         x = newXY[0];
@@ -21,6 +29,10 @@ public class CarDecorator extends VehicleDecorator{
         GameManager.changeBoardField(x, y, symbol);
         return true;
     }
+    /**
+     * Pozwala na opuszczenie samochodu. 
+     * @return true jeśli po opuszczeniu samochodu pieszy nie wpada na inny element, false w przeciwnym przypadku.
+     */
     public boolean getOff(){
         System.out.println("WYSIADL Z AUTA");
         Pedestrian walker = new Pedestrian(x,y);
@@ -30,6 +42,10 @@ public class CarDecorator extends VehicleDecorator{
         GameManager.changeBoardField(x, y, symbol);
         return done;
     }
+    /**
+     * Zwraca prędkość samochodu.
+     * @return prędkość samochodu. 
+     */
     public static int getSpeed(){
         return SPEED;
     }
